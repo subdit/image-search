643e3fbb1442f39cb38c5fac6b520592a2a31a00
Kasidit Bualoeng <subdit@gmail.com>
2023-11-15 12:54:11 -0500

    edite index.css file/add the Css for styling/import Form from React Form/

--- unsplash-search-image/src/App.jsx
+++ unsplash-search-image/src/App.jsx
@@ -1,7 +1,21 @@
+import React from 'react';
+import { Form } from 'react-bootstrap';
 import './index.css';
 
-function App() {
-  return <h3>Hello</h3>;
-}
-
+const App = () => {
+  return (
+    <div className='container'>
+      <h1 className='title'>Image Search</h1>
+      <div className='search-section'>
+        <Form>
+          <Form.Control
+            type='search'
+            placeholder='Enter title to search'
+            className='search-input'
+          />
+        </Form>
+      </div>
+    </div>
+  );
+};
 export default App;
