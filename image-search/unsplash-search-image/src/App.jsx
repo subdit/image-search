9d0ae5b36a81f8ae7f79460afe76156bd5d43520
Kasidit Bualoeng <subdit@gmail.com>
2023-11-15 15:34:53 -0500

    restore new file

---
+++ image-search/unsplash-search-image/src/App.jsx
@@ -0,0 +1,42 @@
+import React, { useRef } from 'react';
+
+import { Form } from 'react-bootstrap';
+import './index.css';
+
+const API_URL = 'https://api.unsplash.com/search/photos?page=1&query=office';
+
+const App = () => {
+  const searchInput = useRef(null); // useRef(hook) to render only one input instead of using useState.
+
+  const handleSearch = event => {
+    event.preventDefault(); // to prevent the refresh in the search box
+
+    // console.log(searchInput.current.value); // handle the form submission
+  };
+  const handleSelection = selection => {
+    searchInput.current.value = selection;
+  };
+
+  return (
+    <div className='container'>
+      <h1 className='title'>Search Image</h1>
+      <div className='search-section'>
+        <Form onSubmit={handleSearch}>
+          <Form.Control
+            type='search'
+            placeholder='Enter title to search..'
+            className='search-input'
+            ref={searchInput}
+          />
+        </Form>
+      </div>
+      <div className='filters'>
+        <div onClick={() => handleSelection('nature')}>Nature</div>
+        <div onClick={() => handleSelection('birds')}>Birds</div>
+        <div onClick={() => handleSelection('cats')}>Cats</div>
+        <div onClick={() => handleSelection('Shoes')}>Shoes</div>
+      </div>
+    </div>
+  );
+};
+export default App;
