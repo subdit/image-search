a2c6cdc22c0a47a3f3e59815b5412615a297a676
Kasidit Bualoeng <subdit@gmail.com>
2023-11-15 14:06:44 -0500

    create handleInput using useRef for form submission

--- unsplash-search-image/src/App.jsx
+++ unsplash-search-image/src/App.jsx
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, { useRef } from 'react';
+
 import { Form } from 'react-bootstrap';
 import './index.css';
 
 const App = () => {
+  const searchInput = useRef(null); // useRef(hook) to render only one input instead of using useState.
+
+  const handleSearch = event => {
+    event.preventDefault(); // to prevent the refresh in the search box
+    console.log(searchInput.current.value); // handle the form submission
+  };
+
   return (
     <div className='container'>
-      <h1 className='title'>Image Search</h1>
+      <h1 className='title'>Search Image</h1>
       <div className='search-section'>
-        <Form>
+        <Form onSubmit={handleSearch}>
           <Form.Control
             type='search'
-            placeholder='Enter title to search'
+            placeholder='Enter title to search..'
             className='search-input'
+            ref={searchInput}
           />
         </Form>
       </div>
