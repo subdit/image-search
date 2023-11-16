b9d7168fba98e0009f4f5e96bfecd55452e500b5
Kasidit Bualoeng <subdit@gmail.com>
2023-11-16 13:32:22 -0500

    created fetchImages valiable

--- image-search/unsplash-search-image/src/App.jsx
+++ image-search/unsplash-search-image/src/App.jsx
@@ -1,14 +1,31 @@
-// import axios from 'axios';
+import axios from 'axios';
 import { useRef } from 'react';
 
 import { Form } from 'react-bootstrap';
 import './index.css';
 
-// const API_URL = 'https://api.unsplash.com/search/photos?page=1&query=office';
-// const API_URL = 'https://api.unsplash.com/search/photos';
+const API_URL = 'https://api.unsplash.com/search/photos';
+const IMAGEs_PER_PAGE = 20;
 
 const App = () => {
+  console.log(import.meta.env.VITE_API_KEY);
+
   const searchInput = useRef(null); // useRef(hook) to render only one input instead of using useState.
+
+  const fetchImages = async () => {
+    try {
+      const result = await axios.get(
+        `${API_URL}?query=${
+          searchInput.current.value
+        }&page=1&per_page=${IMAGEs_PER_PAGE}&client_id=${
+          import.meta.env.VITE_API_KEY
+        }`
+      );
+      console.log('result', result.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   const handleSearch = event => {
     event.preventDefault(); // to prevent the refresh in the search box
@@ -16,11 +33,13 @@ const App = () => {
   };
   const handleSelection = selection => {
     searchInput.current.value = selection;
+    fetchImages();
   };
 
   return (
     <div className='container'>
       <h1 className='title'>Search Image By Unsplash</h1>
+      <h3>Hello..!</h3>
       <div className='search-section'>
         <Form onSubmit={handleSearch} type='text' id='name'>
           <Form.Control
